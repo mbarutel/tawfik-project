d975d09320f81faf879f46de071d2ef62bfd11ea
Michel Philippe Edubas <mikeedubas@hotmail.com>
2025-03-21 19:22:27 +1000

    sizes for different viewports on navbar logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
       <div className="container py-4 flex justify-between items-center">
         {/* Logo Placeholder */}
         <Link href="#" className="text-secondary flex items-center">
-          <div className="relative h-14 w-14">
+          <div className="relative h-8 w-8 sm:h-10 sm:w-10 lg:h-14 lg:w-14">
             <Image
               src="/logo.svg"
               alt="Executive Couching & Consultation Logo"
@@ -23,8 +23,10 @@ export default function Navbar() {
             />
           </div>
           <div className="ml-2 hidden sm:block">
-            <h5 className="text-3xl">TAWFIK</h5>
-            <h4 className="text-xl font-light -mt-1">EXECUTIVE COACHING</h4>
+            <h5 className="text-xl sm:text-2xl lg:text-3xl">TAWFIK</h5>
+            <h4 className="text-base sm:text-lg lg:text-xl font-light -mt-2 lg:-mt-1">
+              EXECUTIVE COACHING
+            </h4>
           </div>
         </Link>
 
