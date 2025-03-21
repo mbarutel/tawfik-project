1b31d7862883ca988de5ba4b1649a5f84e21defc
Michel Philippe Edubas <mikeedubas@hotmail.com>
2025-03-21 20:44:40 +1000

    cta and resources for different media sizes

--- sections/home-cta.tsx
+++ sections/home-cta.tsx
@@ -1,32 +1,33 @@
 "use client";
-import { BsArrowRight } from "react-icons/bs";
+import { BsArrowDown, BsArrowRight } from "react-icons/bs";
 import { InlineWidget } from "react-calendly";
 
 export default function HomeCta() {
   return (
     <section className="bg-light">
-      <div className="container grid grid-cols-1 md:grid-cols-2 gap-12 items-center px-6 md:px-12">
-        <div className="text-center md:text-left text-primary">
+      <div className="container grid grid-cols-1 lg:grid-cols-2 gap-2 lg:gap-12 items-center px-6 md:px-12">
+        <div className="text-center lg:text-left text-primary">
           <h2 className="section_header decoration-light">
             Start Your Journey
           </h2>
           <h3 className="text-4xl text-secondary mt-2">
             Book a 30-min Consultation
           </h3>
-          <p className="my-4 leading-relaxed max-w-[60ch]">
+          <p className="my-4 leading-relaxed lg:w-[min(100%,60ch)]">
             Lorem Ipsum is not simply random text. It has roots in a piece of
             classical Latin literature from 45 BC, making it over 2000 years
             old.
           </p>
           <p className="montserrat text-base lg:text-xl">
             Book Now
-            <BsArrowRight className="inline-flex ml-2" />
+            <BsArrowRight className="hidden lg:inline-flex ml-2" />
+            <BsArrowDown className="lg:hidden inline-flex ml-2" />
           </p>
         </div>
-        <div className="py-8">
+        <div className="py-8 h-[550px] lg:h-[450px]">
           <InlineWidget
             url="https://calendly.com/edubas128"
-            styles={{ height: "400px", width: "100%", borderRadius: "10px" }}
+            styles={{ height: "100%", width: "100%", borderRadius: "10px" }}
           />
         </div>
       </div>
--- sections/home-resources.tsx
+++ sections/home-resources.tsx
@@ -4,26 +4,28 @@ import Link from "next/link";
 export default function HomeResources() {
   return (
     <section>
-      <div className="container grid grid-cols-3">
-        <div className="relative h-[450px] w-[120%] col-span-1 my-8">
-          <Image
-            src="/resources.webp"
-            alt="Executive Coaching & Consulting Services Melbourne"
-            fill
-            className="object-cover"
-          />
-        </div>
-        <div className="bg-light text-primary text-right px-6 flex-grow flex flex-col items-end justify-center col-span-2">
-          <h2 className="section_header italic decoration-light">
-            Recommended Reading
-          </h2>
-          <p className="w-[60ch] my-8">
-            Contrary to popular belief, Lorem Ipsum popular belief popular
-            belief Contrary to popular belief, Lorem Ipsum popular belief{" "}
-          </p>
-          <Link href="#" className="button border-primary">
-            Learn More
-          </Link>
+      <div className="container">
+        <div className="xl:grid grid-cols-3 bg-light/60 xl:bg-light/0 p-4 rounded-md mx-auto">
+          <div className="relative h-[450px] w-full xl:w-[120%] col-span-1 xl:my-8">
+            <Image
+              src="/resources.webp"
+              alt="Executive Coaching & Consulting Services Melbourne"
+              fill
+              className="object-cover rounded-t-md xl:rounded-none"
+            />
+          </div>
+          <div className="bg-light text-primary text-center xl:text-right px-6 flex-grow flex flex-col items-center xl:items-end justify-center col-span-2 py-4 rounded-b-md xl:rounded-none">
+            <h2 className="section_header italic decoration-light">
+              Recommended Reading
+            </h2>
+            <p className="w-[min(100%,60ch)] my-5 xl:my-8">
+              Contrary to popular belief, Lorem Ipsum popular belief popular
+              belief Contrary to popular belief, Lorem Ipsum popular belief{" "}
+            </p>
+            <Link href="#" className="button border-primary">
+              Learn More
+            </Link>
+          </div>
         </div>
       </div>
     </section>
