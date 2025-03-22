18092ce31a81fdecaae1d7401f6d014b2ffdd4e9
Michel Philippe Edubas <mikeedubas@hotmail.com>
2025-03-22 14:56:56 +1000

    slight adjustment on home resources

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default async function Home() {
       <HomeServices />
       <HomeQuotes />
       <HomeCta />
-      <HomeResources blog={blog} />
+      <HomeResources />
     </>
   );
 }
--- sections/home-resources.tsx
+++ sections/home-resources.tsx
@@ -1,28 +1,23 @@
-import { BlogType } from "@/contentful/utils/types";
 import Image from "next/image";
 import Link from "next/link";
 
-type HomeResourcesProps = {
-  blog: BlogType;
-};
-export default function HomeResources({ blog }: HomeResourcesProps) {
+export default function HomeResources() {
   return (
     <section>
       <div className="container">
-        <div className="xl:grid grid-cols-3 bg-light/60 xl:bg-light/0 p-4 rounded-md mx-auto">
-          <div className="relative h-[450px] w-full xl:w-[120%] col-span-1 xl:my-8">
+        <div className="xl:grid grid-cols-3 p-4 rounded-md mx-auto">
+          <div className="hidden xl:block relative h-[450px] w-[110%] col-span-1 my-8">
             <Image
-              src={blog.poster.src}
-              alt={blog.poster.alt}
+              src="/resources.webp"
+              alt="Executive Coaching Services Melbourne"
               fill
-              className="object-cover rounded-t-md xl:rounded-none"
+              className="object-cover"
             />
           </div>
-          <div className="bg-light text-primary text-center xl:text-right px-6 flex-grow flex flex-col items-center xl:items-end justify-center col-span-2 py-4 rounded-b-md xl:rounded-none">
+          <div className="bg-light text-primary text-center xl:text-right px-6 flex-grow flex flex-col items-center xl:items-end justify-center col-span-2 py-8 xl:py-4">
             <h2 className="section_header italic decoration-light">
               Recommended Reading
             </h2>
-            <h3>{blog.title}</h3>
             <p className="w-[min(100%,60ch)] my-5 xl:my-8">
               Contrary to popular belief, Lorem Ipsum popular belief popular
               belief Contrary to popular belief, Lorem Ipsum popular belief{" "}
