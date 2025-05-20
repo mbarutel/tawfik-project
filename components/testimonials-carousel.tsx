e92a6f7f86466c1382b2d3977cd442059b4a93b0
Michel Philippe Edubas <mikeedubas@hotmail.com>
2025-05-20 16:40:34 +1000

    update

--- components/testimonials-carousel.tsx
+++ components/testimonials-carousel.tsx
@@ -2,6 +2,7 @@
 
 import { TestimonialType } from "@/contentful/utils/types";
 import { usePrevNextButtons } from "@/lib/carousel-utils";
+import { clsx } from "clsx";
 import useEmblaCarousel from "embla-carousel-react";
 import { FaCircleChevronLeft, FaCircleChevronRight } from "react-icons/fa6";
 import { IoIosBriefcase } from "react-icons/io";
@@ -12,7 +13,7 @@ export default function TestimonialsCarousel({
 }: {
   testimonials: TestimonialType[];
 }) {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
 
   const {
     prevBtnDisabled,
@@ -22,45 +23,55 @@ export default function TestimonialsCarousel({
   } = usePrevNextButtons(emblaApi);
 
   return (
-    <div className="bg-primary/90 rounded-sm py-24 px-8">
-      <div className="flex justify-between mb-6">
-        <h2 className="section_header !no-underline">Testimonials</h2>
-        <div className="flex gap-3">
-          <PrevButton
-            onClick={() => onPrevButtonClick()}
-            disabled={prevBtnDisabled}
-          />
-          <NextButton
-            onClick={() => onNextButtonClick()}
-            disabled={nextBtnDisabled}
-          />
+    <div className="bg-primary/90 rounded-sm py-18 px-8">
+      <h2 className="section_header text-center !no-underline">Testimonials</h2>
+      <div className="overflow-hidden relative my-6" ref={emblaRef}>
+        <div className="flex -ml-3">
+          {testimonials.map((testimonial, index) => {
+            const initials = GetInitials(testimonial.name);
+
+            return (
+              <div
+                key={index}
+                className="flex-grow-0 flex-shrink-0 w-full lg:w-1/3 cursor-grab active:cursor-grabbing pl-3"
+              >
+                <div className="bg-light/80 rounded-sm h-full text-primary px-6 py-8 flex flex-col shadow-2xl">
+                  <p className="mb-4 !text-xl">"{testimonial.testimony}"</p>
+                  <p className="italic !text-xl">
+                    - {initials}, {testimonial.position}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
-      <div className="overflow-hidden relative" ref={emblaRef}>
-        <div className="flex -ml-3">
-          {testimonials.map((testimonial, index) => (
-            <div
-              key={index}
-              className="flex-grow-0 flex-shrink-0 w-1/3 cursor-grab active:cursor-grabbing pl-3"
-            >
-              <div className="bg-light rounded-md h-full text-primary px-6 py-8 flex flex-col shadow-2xl">
-                <p className="mb-4 !text-xl">"{testimonial.testimony}"</p>
-                <h3 className="text-2xl mt-auto font-bold flex items-center gap-1">
-                  <IoPerson className="inline-flex" />
-                  {testimonial.name}
-                </h3>
-                <h3 className="text-xl italic font-light flex items-center gap-1">
-                  <IoIosBriefcase className="inline-flex" />
-                  {testimonial.position}
-                </h3>
-              </div>
-            </div>
-          ))}
-        </div>
+      <div className="flex gap-3 justify-center">
+        <PrevButton
+          onClick={() => onPrevButtonClick()}
+          disabled={prevBtnDisabled}
+        />
+        <NextButton
+          onClick={() => onNextButtonClick()}
+          disabled={nextBtnDisabled}
+        />
       </div>
     </div>
   );
 }
+
+function GetInitials(name: string): string {
+  const nameSplit = name.split(" ");
+  return `${nameSplit[0][0]}.${nameSplit[nameSplit.length - 1][0]}.`;
+}
+// <h3 className="text-lg mt-auto font-bold flex items-center gap-1">
+//   <IoPerson className="inline-flex" />
+//   {testimonial.name}
+// </h3>
+// <h3 className="text-lg italic font-light flex items-center gap-1">
+//   <IoIosBriefcase className="inline-flex" />
+//   {testimonial.position}
+// </h3>
 
 type PropType = {
   onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
@@ -73,7 +84,9 @@ function PrevButton({ onClick, disabled }: PropType) {
       type="button"
       onClick={onClick}
       disabled={disabled}
-      className="text-4xl"
+      className={clsx("text-4xl transition", {
+        "opacity-80 cursor-not-allowed": disabled,
+      })}
     >
       <FaCircleChevronLeft />
     </button>
@@ -86,7 +99,9 @@ function NextButton({ onClick, disabled }: PropType) {
       type="button"
       onClick={onClick}
       disabled={disabled}
-      className="text-4xl"
+      className={clsx("text-4xl transition", {
+        "opacity-80 cursor-not-allowed": disabled,
+      })}
     >
       <FaCircleChevronRight />
     </button>
